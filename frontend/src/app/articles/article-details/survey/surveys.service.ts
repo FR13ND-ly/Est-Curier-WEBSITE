cf7b5531d7d82a563440958842029c7ffdd49a3a
Alan <57838113+FR13ND-ly@users.noreply.github.com>
2021-12-24 16:43:11 +0200

    Create surveys.service.ts

---
+++ frontend/src/app/articles/article-details/survey/surveys.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { config } from '../../../config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SurveysService {
+
+  constructor(private http: HttpClient) {}
+  readonly APIUrl = config.apiUrl
+
+  getSurvey(data: any){
+    return this.http.post(this.APIUrl + 'getSurvey/', data).toPromise()
+  }
+
+  vote(data: any) {
+    return this.http.post(this.APIUrl + 'vote/', data).toPromise()
+  }
+}
