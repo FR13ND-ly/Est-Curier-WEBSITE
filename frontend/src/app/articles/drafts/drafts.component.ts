56cf36d7da1e7e5e5e4fe18839f3aa5946e5a32d
Alan <57838113+FR13ND-ly@users.noreply.github.com>
2021-12-24 15:18:18 +0200

    refactored drafts component

--- frontend/src/app/articles/drafts/drafts.component.ts
+++ frontend/src/app/articles/drafts/drafts.component.ts
@@ -5,28 +5,31 @@ import { UserService } from 'src/app/user.service';
 import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-drafts',
-  templateUrl: './drafts.component.html',
-  styleUrls: ['./drafts.component.scss']
+    selector: 'app-drafts',
+    templateUrl: './drafts.component.html',
+    styleUrls: ['./drafts.component.scss'],
 })
 export class DraftsComponent implements OnInit, OnDestroy {
+    constructor(
+        private articleService: ArticlesService,
+        private userService: UserService,
+        private router: Router
+    ) {}
 
-  constructor(private articleService: ArticlesService, private userService : UserService, private router : Router) { }
-  
-  articles: any = []
-  private userSub: Subscription | undefined;
-  async ngOnInit() {
-    this.userSub = this.userService.getUserUpdateListener()
-      .subscribe(async (user: any) => {
-        if (!user || !user.isStaff){
-          this.router.navigate(['/'])
-        }  
-        this.articles = await this.articleService.getDrafts()
-      })
-  }
+    articles: any = [];
+    private userSub: Subscription | undefined;
+    async ngOnInit() {
+        this.userSub = this.userService
+            .getUserUpdateListener()
+            .subscribe(async (user: any) => {
+                if (!user || !user.isStaff) {
+                    this.router.navigate(['/']);
+                }
+                this.articles = await this.articleService.getDrafts();
+            });
+    }
 
-  ngOnDestroy() {
-    this.userSub?.unsubscribe()
-  }
-
+    ngOnDestroy() {
+        this.userSub?.unsubscribe();
+    }
 }
