7b0c48e809ed58ed450c290ded82ad6601859310
Alan <57838113+FR13ND-ly@users.noreply.github.com>
2021-12-24 16:53:08 +0200

    refactored survey component

--- frontend/src/app/articles/article-details/survey/survey.component.ts
+++ frontend/src/app/articles/article-details/survey/survey.component.ts
@@ -3,39 +3,49 @@ import { SurveysService } from 'src/app/articles/article-details/survey/surveys.
 import { UserService } from 'src/app/user.service';
 
 @Component({
-  selector: 'app-survey',
-  templateUrl: './survey.component.html',
-  styleUrls: ['./survey.component.scss']
+    selector: 'app-survey',
+    templateUrl: './survey.component.html',
+    styleUrls: ['./survey.component.scss'],
 })
 export class SurveyComponent implements OnInit {
+    @Input() id: any = '';
 
-  @Input() id : any = ''
-
-  constructor(private surveyService: SurveysService, private userService : UserService) { }
-  surveys : any
-  user : any
-  selected : any = 0
-   ngOnInit() {
-    this.userService.getUserUpdateListener()
-      .subscribe(async (user: any) => {
-        this.user = user
-        this.surveys = await this.surveyService.getSurvey({id : this.id, token : this.user ? this.user.uid : -1 })
-      })
-  }
-
-  changeSurvey(direction: any) {
-    if (direction) {
-      this.selected = this.selected == 0 ? this.surveys.length - 1 : --this.selected
+    constructor(
+        private surveyService: SurveysService,
+        private userService: UserService
+    ) {}
+    surveys: any;
+    user: any;
+    selected: any = 0;
+    ngOnInit() {
+        this.userService
+            .getUserUpdateListener()
+            .subscribe(async (user: any) => {
+                this.user = user;
+                this.surveys = await this.surveyService.getSurvey({
+                    id: this.id,
+                    token: this.user ? this.user.uid : -1,
+                });
+            });
     }
-    else {
-      this.selected = this.selected == this.surveys.length - 1 ? 0 : ++this.selected
-    }
-  }
 
-  async onVote(id : any) {
-    if (this.user){
-      await this.surveyService.vote({ id, token : this.user.uid })
-      this.surveys = await this.surveyService.getSurvey({id : this.id, token : this.user.uid})
+    changeSurvey(direction: any) {
+        if (direction) {
+            this.selected =
+                this.selected == 0 ? this.surveys.length - 1 : --this.selected;
+        } else {
+            this.selected =
+                this.selected == this.surveys.length - 1 ? 0 : ++this.selected;
+        }
     }
-  }
+
+    async onVote(id: any) {
+        if (this.user) {
+            await this.surveyService.vote({ id, token: this.user.uid });
+            this.surveys = await this.surveyService.getSurvey({
+                id: this.id,
+                token: this.user.uid,
+            });
+        }
+    }
 }
--- frontend/src/app/articles/article-details/survey/surveys.service.ts
+++ frontend/src/app/articles/article-details/survey/surveys.service.ts
@@ -3,18 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { config } from '../../../config';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class SurveysService {
+    constructor(private http: HttpClient) {}
+    readonly APIUrl = config.apiUrl;
 
-  constructor(private http: HttpClient) {}
-  readonly APIUrl = config.apiUrl
+    getSurvey(data: any) {
+        return this.http.post(this.APIUrl + 'getSurvey/', data).toPromise();
+    }
 
-  getSurvey(data: any){
-    return this.http.post(this.APIUrl + 'getSurvey/', data).toPromise()
-  }
-
-  vote(data: any) {
-    return this.http.post(this.APIUrl + 'vote/', data).toPromise()
-  }
+    vote(data: any) {
+        return this.http.post(this.APIUrl + 'vote/', data).toPromise();
+    }
 }
