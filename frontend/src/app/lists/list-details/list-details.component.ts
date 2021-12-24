aaac32906b6510d77e3a41a185c313df0fa29167
Alan <57838113+FR13ND-ly@users.noreply.github.com>
2021-12-24 15:03:46 +0200

    refactored list details component

--- frontend/src/app/lists/list-details/list-details.component.ts
+++ frontend/src/app/lists/list-details/list-details.component.ts
@@ -2,79 +2,92 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ListsService } from 'src/app/lists/lists.service';
 import { UserService } from 'src/app/user.service';
-import { first } from 'rxjs/operators'
+import { first } from 'rxjs/operators';
 import { LoadingService } from 'src/app/loading.service';
 
 @Component({
-  selector: 'app-list-details',
-  templateUrl: './list-details.component.html',
-  styleUrls: ['./list-details.component.scss']
+    selector: 'app-list-details',
+    templateUrl: './list-details.component.html',
+    styleUrls: ['./list-details.component.scss'],
 })
 export class ListDetailsComponent implements OnInit {
-
-  constructor(private listService : ListsService, private route: ActivatedRoute, private userService: UserService, private router: Router, private loadingService : LoadingService) {}
-  listInfo : any = {
-  }
-  articles : any = []
-  listId: any = ''
-  user : any = ''
-  loading : boolean = true
-  index = 1
-  noMoreArticles = true
-  async ngOnInit() {
-    this.loadingService.setLoading(true)
-    let params: any = await this.route.params.pipe(first()).toPromise()
-    this.listId = params['id']
-    if (this.listId == "istoric"){
-      this.listId = -1
+    constructor(
+        private listService: ListsService,
+        private route: ActivatedRoute,
+        private userService: UserService,
+        private router: Router,
+        private loadingService: LoadingService
+    ) {}
+    listInfo: any = {};
+    articles: any = [];
+    listId: any = '';
+    user: any = '';
+    loading: boolean = true;
+    index = 1;
+    noMoreArticles = true;
+    async ngOnInit() {
+        this.loadingService.setLoading(true);
+        let params: any = await this.route.params.pipe(first()).toPromise();
+        this.listId = params['id'];
+        if (this.listId == 'istoric') {
+            this.listId = -1;
+        } else if (this.listId == 'aprecieri') {
+            this.listId = -2;
+        }
+        this.user = await this.userService
+            .getUserUpdateListener()
+            .pipe(first())
+            .toPromise();
+        if (this.listId) {
+            this.onGetList();
+        }
     }
-    else if (this.listId == "aprecieri"){
-      this.listId = -2
+
+    async onGetList() {
+        this.listInfo = await this.listService.getListInfo({
+            id: this.listId,
+            token: this.user.uid,
+        });
+
+        if (this.listId > 0) {
+            if (this.listInfo == 'Nu am găsit această listă') {
+                this.router.navigate(['/']);
+                return;
+            }
+            if (!this.listInfo.own && this.listInfo.public) {
+                this.router.navigate(['/']);
+                return;
+            }
+        }
+        let data: any = await this.listService.getListArticles({
+            id: this.listId,
+            token: this.user.uid,
+            index: this.index,
+        });
+        this.articles.push(...data.articles);
+        this.noMoreArticles = data.noMoreArticles;
+        this.loading = false;
+        this.loadingService.setLoading(false);
+        this.index++;
     }
-    this.user = await this.userService.getUserUpdateListener().pipe(first()).toPromise()
-    if (this.listId){
-      this.onGetList()
+
+    onRemoveList() {
+        if (confirm('Ești sigur că dorești să ștergi lista?')) {
+            this.listService.removeList(this.listId);
+            this.router.navigate(['/']);
+        }
     }
-  }
 
-  async onGetList() {
-    this.listInfo = await this.listService.getListInfo({id : this.listId, token : this.user.uid})
-    
-    if (this.listId > 0){
-      if (this.listInfo == "Nu am găsit această listă"){
-        this.router.navigate(['/'])
-        return
-      }
-      if (!this.listInfo.own && this.listInfo.public){
-        this.router.navigate(['/'])
-        return
-      }
+    onChangeTitle(title: any) {
+        this.listService.changeTitle({ id: this.listId, name: title.trim() });
     }
-    let data : any = await this.listService.getListArticles({id : this.listId, token : this.user.uid, index : this.index})
-    this.articles.push(...data.articles)
-    this.noMoreArticles = data.noMoreArticles
-    this.loading = false
-    this.loadingService.setLoading(false)
-    this.index++;
-  }
 
-  onRemoveList() {
-    if (confirm ("Ești sigur că dorești să ștergi lista?")){
-      this.listService.removeList(this.listId)
-      this.router.navigate(["/"])
+    onChangePublic(publicity: any) {
+        this.listInfo.public = !publicity;
+        this.listService.changePublic({ id: this.listId, publicity });
     }
-  }
 
-  onChangeTitle(title : any) {
-    this.listService.changeTitle({id : this.listId, name: title.trim()})
-  }
-
-  onChangePublic(publicity : any) {
-    this.listInfo.public = !publicity
-    this.listService.changePublic({id : this.listId, publicity})
-  }
-
-  copyLink() {
-    navigator.clipboard.writeText(window.location.href)
-  }
+    copyLink() {
+        navigator.clipboard.writeText(window.location.href);
+    }
 }
