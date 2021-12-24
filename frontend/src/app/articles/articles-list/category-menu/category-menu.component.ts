5e7106c69518347a19a91c25806e9d13b5d9ae68
Alan <57838113+FR13ND-ly@users.noreply.github.com>
2021-12-24 16:21:32 +0200

    refactored category menu

--- frontend/src/app/articles/articles-list/category-menu/category-menu.component.ts
+++ frontend/src/app/articles/articles-list/category-menu/category-menu.component.ts
@@ -3,26 +3,32 @@ import { ArticlesService } from 'src/app/articles.service';
 import { WidgetsService } from 'src/app/widgets/widgets.service';
 
 @Component({
-  selector: 'app-category-menu',
-  templateUrl: './category-menu.component.html',
-  styleUrls: ['./category-menu.component.scss']
+    selector: 'app-category-menu',
+    templateUrl: './category-menu.component.html',
+    styleUrls: ['./category-menu.component.scss'],
 })
 export class CategoryMenuComponent implements OnInit {
+    constructor(
+        private articleService: ArticlesService,
+        private widgetService: WidgetsService
+    ) {}
 
-  constructor(private articleService : ArticlesService, private widgetService: WidgetsService) { }
+    selected: string = 'economic';
+    articles: any = [];
+    widget: any;
+    loading: boolean = true;
+    async ngOnInit() {
+        this.articles = await this.articleService.getCategoryArticles(
+            this.selected
+        );
+        this.widget = await this.widgetService.getWidget(2);
+        this.loading = false;
+    }
 
-  selected: string = "economic"
-  articles: any = []
-  widget: any
-  loading: boolean = true
-  async ngOnInit() {
-    this.articles = await this.articleService.getCategoryArticles(this.selected)
-    this.widget = await this.widgetService.getWidget(2)
-    this.loading = false
-  }
-
-  async onSelectCategory(category: string){
-    this.selected = category
-    this.articles = await this.articleService.getCategoryArticles(this.selected)
-  }
+    async onSelectCategory(category: string) {
+        this.selected = category;
+        this.articles = await this.articleService.getCategoryArticles(
+            this.selected
+        );
+    }
 }
