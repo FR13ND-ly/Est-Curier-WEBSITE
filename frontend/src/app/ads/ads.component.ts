e943311ee816622fada9b3b2f9dc65f868076803
Alan <57838113+FR13ND-ly@users.noreply.github.com>
2021-12-24 15:28:35 +0200

    refactored ads component

--- frontend/src/app/ads/ads.component.ts
+++ frontend/src/app/ads/ads.component.ts
@@ -1,56 +1,59 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AdsService } from '../ads.service';
 import { UserService } from '../user.service';
-import { Subscription } from 'rxjs'
+import { Subscription } from 'rxjs';
 import { Ad } from '../models/ad';
 import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-ads',
-  templateUrl: './ads.component.html',
-  styleUrls: ['./ads.component.scss']
+    selector: 'app-ads',
+    templateUrl: './ads.component.html',
+    styleUrls: ['./ads.component.scss'],
 })
 export class AdsComponent implements OnInit, OnDestroy {
+    constructor(
+        private adService: AdsService,
+        private accountService: UserService
+    ) {}
 
-  constructor(private adService : AdsService, private accountService : UserService) { }
+    ads: Ad[] | undefined;
 
-  ads : Ad[] | undefined
+    user: any = false;
 
-  user: any = false
-  
-  private userSub: Subscription | undefined;
+    private userSub: Subscription | undefined;
 
-  ngOnInit(): void {
-    this.onGetAds()
-    this.user = this.accountService.getUser()
-    this.userSub = this.accountService.getUserUpdateListener()
-      .subscribe((user: any) => {
-        this.user = user
-      }) 
-  }
+    ngOnInit(): void {
+        this.onGetAds();
+        this.user = this.accountService.getUser();
+        this.userSub = this.accountService
+            .getUserUpdateListener()
+            .subscribe((user: any) => {
+                this.user = user;
+            });
+    }
 
-  ngOnDestroy() {
-    this.userSub?.unsubscribe()
-  }
+    ngOnDestroy() {
+        this.userSub?.unsubscribe();
+    }
 
-  async onSaveChanges(ad : Ad) {
-    await this.adService.editAd(ad)
-    ad.edit = false
-    this.onGetAds()
-  }
+    async onSaveChanges(ad: Ad) {
+        await this.adService.editAd(ad);
+        ad.edit = false;
+        this.onGetAds();
+    }
 
-  async onAddAd(ad : NgForm){
-    await this.adService.addAd(ad.value)
-    ad.reset()
-    this.onGetAds()
-  }
+    async onAddAd(ad: NgForm) {
+        await this.adService.addAd(ad.value);
+        ad.reset();
+        this.onGetAds();
+    }
 
-  async onGetAds() {
-    this.ads = <Ad[]>await this.adService.getAds()
-  }
+    async onGetAds() {
+        this.ads = <Ad[]>await this.adService.getAds();
+    }
 
-  async onRemoveAd(id : number) {
-    await this.adService.removeAd(id)
-    this.onGetAds()    
-  }
+    async onRemoveAd(id: number) {
+        await this.adService.removeAd(id);
+        this.onGetAds();
+    }
 }
