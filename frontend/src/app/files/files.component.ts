d6592ee8e66092c7b68d7ebf6f43aa6be921f3fb
Alan <57838113+FR13ND-ly@users.noreply.github.com>
2021-12-24 15:07:46 +0200

    refactored files component

--- frontend/src/app/files/files.component.ts
+++ frontend/src/app/files/files.component.ts
@@ -5,46 +5,50 @@ import { UserService } from 'src/app/user.service';
 import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-files',
-  templateUrl: './files.component.html',
-  styleUrls: ['./files.component.scss']
+    selector: 'app-files',
+    templateUrl: './files.component.html',
+    styleUrls: ['./files.component.scss'],
 })
 export class FilesComponent implements OnInit, OnDestroy {
+    constructor(
+        private fileService: FileService,
+        private userService: UserService,
+        private router: Router
+    ) {}
+    files: any = [];
+    index: number = 1;
+    noMoreFiles: boolean = false;
+    loading: boolean = true;
+    private userSub: Subscription | undefined;
+    async ngOnInit() {
+        this.userSub = this.userService
+            .getUserUpdateListener()
+            .subscribe(async (user: any) => {
+                if (!user || !user.isStaff) {
+                    this.router.navigate(['/']);
+                }
+                this.getFiles();
+            });
+    }
 
-  constructor(private fileService : FileService, private userService : UserService, private router : Router) { }
-  files: any = []
-  index : number = 1
-  noMoreFiles : boolean = false
-  loading: boolean = true
-  private userSub: Subscription | undefined;
-  async ngOnInit() {
-    this.userSub = this.userService.getUserUpdateListener()
-      .subscribe(async (user: any) => {
-        if (!user || !user.isStaff){
-          this.router.navigate(['/'])
-        }  
-        this.getFiles()
-      })
-  }
+    ngOnDestroy() {
+        this.userSub?.unsubscribe();
+    }
 
-  ngOnDestroy() {
-    this.userSub?.unsubscribe()
-  }
+    async getFiles() {
+        let data: any = await this.fileService.getFiles(this.index);
+        this.files.push(...data.files);
+        this.noMoreFiles = data.noMoreFiles;
+        this.loading = false;
+    }
 
-  async getFiles() {
-    let data : any = await this.fileService.getFiles(this.index)
-    this.files.push(...data.files)
-    this.noMoreFiles = data.noMoreFiles
-    this.loading = false
-  }
+    async onRemoveFile(id: number) {
+        await this.fileService.removeFile(id);
+        this.files = await this.fileService.getFiles(this.index);
+    }
 
-  async onRemoveFile(id: number) {
-    await this.fileService.removeFile(id)
-    this.files = await this.fileService.getFiles(this.index)
-  }
-
-  onGetMoreFiles() {
-    this.index++
-    this.getFiles()
-  }
+    onGetMoreFiles() {
+        this.index++;
+        this.getFiles();
+    }
 }
