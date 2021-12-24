660e7a0fbe6ca42b593fc528f503df5d2c86c6cb
Alan <57838113+FR13ND-ly@users.noreply.github.com>
2021-12-24 16:47:01 +0200

    refactored more-articles component

--- frontend/src/app/articles/article-details/more-articles/more-articles.component.ts
+++ frontend/src/app/articles/article-details/more-articles/more-articles.component.ts
@@ -2,16 +2,16 @@ import { Component, OnInit, Input } from '@angular/core';
 import { WidgetsService } from 'src/app/widgets/widgets.service';
 
 @Component({
-  selector: 'app-more-articles',
-  templateUrl: './more-articles.component.html',
-  styleUrls: ['./more-articles.component.scss']
+    selector: 'app-more-articles',
+    templateUrl: './more-articles.component.html',
+    styleUrls: ['./more-articles.component.scss'],
 })
 export class MoreArticlesComponent implements OnInit {
-  @Input() articles : any = []
-  widget: any
-  constructor(private widgetService: WidgetsService) { }
-  async ngOnInit() {
-    this.widget = await this.widgetService.getWidget(2)
-  }
+    @Input() articles: any = [];
+    widget: any;
+    constructor(private widgetService: WidgetsService) {}
 
+    async ngOnInit() {
+        this.widget = await this.widgetService.getWidget(2);
+    }
 }
