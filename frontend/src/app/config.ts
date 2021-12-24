786267596e827f48f61d367af4bdff8386443905
Alan <57838113+FR13ND-ly@users.noreply.github.com>
2021-12-24 13:56:54 +0200

    Update config.ts

--- frontend/src/app/config.ts
+++ frontend/src/app/config.ts
@@ -1,3 +1,11 @@
 export const config = {
-    "apiUrl" : "http://127.0.0.1:8000/api/"
+    apiUrl: 'http://127.0.0.1:8000/api/',
+    firebaseConfig: {
+        apiKey: 'AIzaSyCVAEO0vg3nYGEMfk8ptDcbmYhu1dqAj24',
+        authDomain: 'escu-ebe2e.firebaseapp.com',
+        projectId: 'escu-ebe2e',
+        storageBucket: 'escu-ebe2e.appspot.com',
+        messagingSenderId: '936239440609',
+        appId: '1:936239440609:web:9652233531da143e2bcda0',
+    }
 };
