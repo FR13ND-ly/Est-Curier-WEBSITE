7e7cfab33cfecca7d4a2c8e37cd412190c7504ce
Alan <57838113+FR13ND-ly@users.noreply.github.com>
2021-12-24 16:56:47 +0200

    Create ads.service.ts

---
+++ frontend/src/app/ads/ads.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { config } from '../config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdsService {
+
+  constructor(private http: HttpClient) { }
+
+  readonly APIUrl = config.apiUrl
+
+  getAds(){
+    return this.http.get(`${this.APIUrl}getAdons/`).toPromise()
+  }
+
+  addAd(data: any){
+    return this.http.post(`${this.APIUrl}addAdon/`, data).toPromise()
+  }
+
+  editAd(data: any){
+    return this.http.post(`${this.APIUrl}editAdon/`, data).toPromise()
+  }
+
+  removeAd(id: any){
+    return this.http.get(`${this.APIUrl}removeAdon/${id}/`).toPromise()
+  }
+}
