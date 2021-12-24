79328794f42a7898df941c99851c4c68238cfa99
Alan <57838113+FR13ND-ly@users.noreply.github.com>
2021-12-24 13:56:57 +0200

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,17 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import firebase from 'firebase/app';
+import { config } from './app/config'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCVAEO0vg3nYGEMfk8ptDcbmYhu1dqAj24",
-  authDomain: "escu-ebe2e.firebaseapp.com",
-  projectId: "escu-ebe2e",
-  storageBucket: "escu-ebe2e.appspot.com",
-  messagingSenderId: "936239440609",
-  appId: "1:936239440609:web:9652233531da143e2bcda0"
-};
-
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(config.firebaseConfig);
 
 if (environment.production) {
   enableProdMode();
