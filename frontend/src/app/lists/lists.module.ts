efa97d660dadac095701ae9072c0c19b921c6eec
Alan <57838113+FR13ND-ly@users.noreply.github.com>
2022-01-01 15:07:17 +0200

    optimization

--- frontend/src/app/lists/lists.module.ts
+++ frontend/src/app/lists/lists.module.ts
@@ -3,6 +3,7 @@ import { ListsComponent } from './lists/lists.component';
 import { ListDetailsComponent } from './list-details/list-details.component';
 import { MaterialModule } from '../material.module';
 import { PreviewArticleModule } from '../preview-article/preview-article.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { PreviewArticleModule } from '../preview-article/preview-article.module'
   ],
   imports: [
     MaterialModule,
-    PreviewArticleModule
+    PreviewArticleModule,
+    CommonModule
   ],
   exports: [
     ListsComponent,
